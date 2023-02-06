7fec8199ea784980876d062be98ee36fd384d5a1
Jaxon Walter <jaxonskywalter@gmail.com>
2023-02-06 14:08:24 -0700

    test3

--- Mission04_jjw227/Models/GradeCalculator.cs
+++ Mission04_jjw227/Models/GradeCalculator.cs
@@ -8,24 +8,25 @@ namespace Mission04_jjw227.Models
 {
     public class GradeCalculator
     {
-        
+        [Required]
         [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public decimal assignments { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public decimal groupProject { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public decimal quizzes { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public decimal midtermExam { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public decimal finalExam { get; set; }
         [Required]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
         public decimal intex { get; set; }
+
 
     }
 }

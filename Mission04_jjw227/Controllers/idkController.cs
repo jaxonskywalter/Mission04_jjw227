248b9aeb529713c11b20912d8e3a5ed4e312af2d
Jaxon Walter <jaxonskywalter@gmail.com>
2023-02-06 13:12:42 -0700

    test2

---
+++ Mission04_jjw227/Controllers/idkController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_jjw227.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_jjw227.Controllers
+{
+    public class idkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult calculator(GradeCalculator model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission04_jjw227/Models/GradeCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_jjw227.Models
+{
+    public class GradeCalculator
+    {
+        
+        [Range(0, 100, ErrorMessage = "Grade must be between 0 and 100")]
+        public decimal assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal groupProject { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal quizzes { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal midtermExam { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal finalExam { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public decimal intex { get; set; }
+
+    }
+}
